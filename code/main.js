e5c95b5b18998dda4c68bda2dd0af089d1114e8d
Denk <denkyuan@hotmail.com>
2021-02-01 18:04:54 +0800

    Hide images

---
+++ code/main.js
@@ -0,0 +1,13 @@
+function ImgCtrl(){
+    var imgId=document.getElementById("hide_1");
+    if(imgId.style.display=="block")
+        imgId.style.display="none";
+    else
+        imgId.style.display="block";
+
+    var imgId2=document.getElementById("hide_2");
+    if(imgId2.style.display=="block")
+        imgId2.style.display="none";
+    else
+        imgId2.style.display="block";
+}
